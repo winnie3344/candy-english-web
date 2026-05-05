7cafd40d59e8686a9b4083188dd021ba00d1bc09
winnie3344 <397304537@qq.com>
2026-05-05 15:59:22 +0800

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
+})
